f7e762145de373776cdecc61904394b0e506cf52
Ante Koceić <ante@crocoder.dev>
2026-03-18 14:32:52 +0100

    Updated env.example and env.d.ts

--- src/app/env.d.ts
+++ src/app/env.d.ts
@@ -1,8 +1,9 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     RESEND_API_KEY: string;
+    RESEND_SEGMENT_ID: string;
+    NEWSLETTER_ADMIN_SECRET: string;
     FORM_EMAIL_FROM: string;
     FORM_EMIAL_TO: string;
-    RESEND_SEGMENT_ID: string;
   }
 }
