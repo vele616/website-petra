c005b289570af38b8c56cda4579f7bf7d37cd1b8
Ante Koceić <ante@crocoder.dev>
2026-03-18 16:32:59 +0100

    chore:env-update (#24)
    * Updated env.example and env.d.ts
    
    * Removed unused env variable

--- src/app/env.d.ts
+++ src/app/env.d.ts
@@ -1,8 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     RESEND_API_KEY: string;
+    RESEND_SEGMENT_ID: string;
     FORM_EMAIL_FROM: string;
     FORM_EMIAL_TO: string;
-    RESEND_SEGMENT_ID: string;
   }
 }
